7b681cb9304e752be55c8193e50a5f333570d26e
Pranav Pise <itspranavpise@gmail.com>
2025-07-29 12:23:42 +0530

    fog, background

--- src/main.js
+++ src/main.js
@@ -2,17 +2,22 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+
+const fog = new THREE.Fog(0xffffff, 1, 10);
+scene.fog = fog;
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 2;
+camera.position.z = 5;
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: "red",
+  color: 0x00ff00,
   transparent: true,
   opacity: 0.5,
   side: THREE.DoubleSide,
